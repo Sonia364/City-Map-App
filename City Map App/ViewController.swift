8feb88354fbd03b487a7dc56024f1689e77600c3
Sonia364 <c0872364@mylambton.ca>
2023-01-19 09:36:19 -0500

    Initial Commit

---
+++ City Map App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  City Map App
+//
+//  Created by Sonia Nain on 2023-01-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
